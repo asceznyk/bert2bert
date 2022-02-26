850beb2c0977f1e5c8f3a6f31c644eb890ed92d1
asceznyk <asceznyk@gmail.com>
2022-02-26 18:56:01 +0530

    ..

--- dataset.py
+++ dataset.py
@@ -15,26 +15,35 @@ def load_tokenizer(save_path='./bert.tokenizer'):
         return tokenizer
     return BertTokenizerFast.from_pretrained(save_path)
 
+def light_load_csv(path, cols, chunksize=1000):
+    df = pd.read_csv(path, usecols=cols, chunksize=chunksize)
+    xdf = pd.DataFrame(columns=cols)
+    for chunk in df:
+        xdf = pd.concat([xdf, chunk])
+    return xdf
+
 class AbsSummary(Dataset):
     def __init__(self, data_path, xcol, ycol, tokenizer, xmax=512, ymax=50):
-        self.df = pd.read_csv(data_path, usecols=[xcol, ycol])
+        self.df = light_load_csv(data_path, [xcol, ycol]) 
         self.xcol = xcol
         self.ycol = ycol
         self.xmax = xmax
         self.ymax = ymax
         self.tokenizer = tokenizer
 
-    def encode_str(self, s, lim, target=0):
+    def encode_str(self, s, lim):
         t = self.tokenizer.encode_plus(s, max_length=lim, 
                                        truncation=True, padding='max_length')
-        return t['input_ids'], t['attention_mask'] if not target else t['input_ids']
+        return t['input_ids'], t['attention_mask'] 
 
     def __len__(self):
         return self.df.shape[0]
 
     def __getitem__(self, idx):
-        x, mask = self.encode_str(self.df.loc[idx, self.xcol], self.xmax)
-        y = torch.tensor(self.encode_str(self.df.loc[idx, self.ycol], self.ymax, target=1))
-        return torch.tensor(x), torch.tensor(mask), y
+        x, xmask = self.encode_str(self.df.loc[idx, self.xcol], self.xmax)
+        y, ymask = self.encode_str(self.df.loc[idx, self.ycol], self.ymax)
+        x, xmask = torch.tensor(x), torch.tensor(xmask)
+        y, ymask = torch.tensor(y), torch.tensor(ymask)
+        return x, xmask, y, ymask, y 
 
 
