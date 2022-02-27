62d0c0b8177097f2c618ec96a37314917767aa6a
asceznyk <asceznyk@gmail.com>
2022-02-27 16:39:57 +0530

    ..

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SEQ_MAX_LEN = 512
 SUM_MAX_LEN = 128
 BATCH_SIZE = 8
 HIDDEN_DIM = 512
-LEARNING_RATE = 1e-5 #1e-4 
+LEARNING_RATE = 1e-4 #1e-5 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
