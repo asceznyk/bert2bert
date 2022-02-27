1c272fa6a40fe8414e0c650ebff411d2d9e68b93
asceznyk <asceznyk@gmail.com>
2022-02-27 18:34:11 +0530

    ..

--- predict.py
+++ predict.py
@@ -13,12 +13,15 @@ def main(args):
 
     tokenizer = load_tokenizer()
     
-    model = EncoderDecoderModel.from_encoder_decoder_pretrained(
-        'bert-base-uncased', 'bert-base-uncased'
-    )
+    if args.ckpt_path:
+        model = EncoderDecoderModel.from_pretrained(args.ckpt_path)
+    else: 
+        model = EncoderDecoderModel.from_encoder_decoder_pretrained(
+            'bert-base-uncased', 'bert-base-uncased'
+        )
     model = warm_start(model, tokenizer).to(device)
     
-    if args.ckpt_path: model.load_state_dict(torch.load(args.ckpt_path))
+    
 
     inputs = tokenizer(text, padding="max_length", truncation=True, max_length=SEQ_MAX_LEN, return_tensors="pt")
     input_ids = inputs.input_ids.to(device)
