0fe0da2df192091985fcd9ad3f253804b2e119a5
asceznyk <asceznyk@gmail.com>
2022-02-27 18:02:50 +0530

    ..

--- fit.py
+++ fit.py
@@ -18,8 +18,8 @@ def fit(model, train_loader, valid_loader=None, ckpt_path=None):
             x, xmask, y, ymask = batch
             
             with torch.set_grad_enabled(is_train):  
-                outputs = model(input_ids=x, attention_mask=xmask 
-                                labels=y, decoder_attention_mask=ymask
+                outputs = model(input_ids=x, attention_mask=xmask, 
+                                labels=y, decoder_attention_mask=ymask,
                                 return_dict=True)
                 loss, logits = outputs.loss, outputs.logits 
                 avg_loss += loss.item() / len(loader)
