97c66d3156df95048f0a528e111b601630aa0e66
asceznyk <asceznyk@gmail.com>
2022-02-28 13:47:12 +0530

    ..

--- train.py
+++ train.py
@@ -12,6 +12,9 @@ from config import *
 from dataset import *
 from fit import *
 
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["WANDB_DISABLED"] = "true"
+
 def main(args):
     train_file, valid_file = args.train_file, args.valid_file
     xcol, ycol = args.xcol, args.ycol
