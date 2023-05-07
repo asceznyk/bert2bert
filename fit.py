0643ec8236bc2d56c225126df270cdf301e58870
asceznyk <asceznyk@gmail.com>
2023-05-07 14:58:29 +0530

    added DataLoader in fit.py

--- fit.py
+++ fit.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 import torch.optim as optim
 
+from torch.utils.data import DataLoader
+
 from transformers import BertTokenizerFast, EncoderDecoderModel
 
 
@@ -25,15 +27,15 @@ def warm_start(model:EncoderDecoderModel, tokenizer:BertTokenizerFast) -> Encode
 
 
 def fit(
-    model:EncoderDecoderModel, 
-    train_loader:DataLoader, 
-    valid_loader:Union[None,DataLoader]=None, 
-    epochs:int=5, 
-    lr:float=1e-5, 
+    model:EncoderDecoderModel,
+    train_loader:DataLoader,
+    valid_loader:Union[None,DataLoader]=None,
+    epochs:int=5,
+    lr:float=1e-5,
     ckpt_path:Union[str,None]=None
-): 
+):
     def run_epoch(split):
-        is_train = split == 'train' 
+        is_train = split == 'train'
         model.train(is_train)
         loader = train_loader if is_train else valid_loader
 
@@ -42,26 +44,26 @@ def fit(
         for step, batch in pbar:
             batch = (v.to(device) for k, v in batch.items())
             x, xmask, labels, y, ymask = batch
-            
+
             with torch.set_grad_enabled(is_train):
-                outputs = model(input_ids=x, attention_mask=xmask, 
+                outputs = model(input_ids=x, attention_mask=xmask,
                                 labels=labels, decoder_attention_mask=ymask,
                                 return_dict=True)
                 loss = outputs.loss
                 avg_loss += loss.item() / len(loader)
 
-            if is_train: 
-                optimizer.zero_grad() 
-                loss.backward() 
-                optimizer.step() 
-                #scheduler.step() 
+            if is_train:
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                #scheduler.step()
 
-            pbar.set_description(f"epoch: {e+1}, loss: {loss.item():.3f}, avg: {avg_loss:.2f}, latest lr: {optimizer.param_groups[0]['lr']}")     
+            pbar.set_description(f"epoch: {e+1}, loss: {loss.item():.3f}, avg: {avg_loss:.2f}, latest lr: {optimizer.param_groups[0]['lr']}")
         return avg_loss
 
     model.to(device)
 
-    best_loss = float('inf') 
+    best_loss = float('inf')
     optimizer = optim.Adam(model.parameters(), lr=lr)
     #scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=1e-3, steps_per_epoch=len(train_loader), epochs=EPOCHS)
     for e in range(epochs):
